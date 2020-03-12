a9487979af09582e7f14149baa7437aa9d4eedff
rafal19987 <rafal987.1@o2.pl>
2020-03-12 12:25:17 +0100

    Dodanie_logowania

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.all('*', (req, res, next) => {
+  if(!req.session.admin){
+    res.redirect('login');
+
+    return;
+  }
+
+  next();
+});
+
+
 /* GET home page. */
 router.get('/', (req, res) => {
+
   res.render('admin', { title: 'Admin' });
 });
 
